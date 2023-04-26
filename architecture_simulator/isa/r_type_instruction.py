8b5ea32f075643bad6315d1cadf99fb1a33a5360
Michael Kuhn <michael.kuhn@uni-tuebingen.de>
2023-04-26 12:31:26 +0200

    remove old files

--- architecture_simulator/isa/r_type_instruction.py
+++
@@ -1,9 +0,0 @@
-from .instruction import Instruction
-
-
-class RTypeInstruction(Instruction):
-    def __init__(self, rs1: int, rs2: int, rd: int, **args):
-        super().__init__(**args)
-        self.rs1 = rs1
-        self.rs2 = rs2
-        self.rd = rd
