b730a075d1aab6bcc7d7a2f3232f42a4984b8e32
Yamin Maazou <yamin.maazou@student.uni-tuebingen.de>
2023-05-06 15:16:41 +0000

    Rough outliers for tests

--- tests/test_isa.py
+++ tests/test_isa.py
@@ -2,7 +2,7 @@ import unittest
 import fixedint
 
 from architecture_simulator.uarch.architectural_state import RegisterFile
-from architecture_simulator.isa.rv32i_instructions import ADD, SUB
+from architecture_simulator.isa.rv32i_instructions import ADD, SUB, ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI
 from architecture_simulator.uarch.architectural_state import ArchitecturalState
 
 from architecture_simulator.isa.parser import riscv_bnf, riscv_parser
@@ -20,6 +20,69 @@ class TestInstructions(unittest.TestCase):
         sub_1 = SUB(rs1=1, rs2=2, rd=0)
         state = sub_1.behavior(state)
         self.assertEqual(state.register_file.registers, [-4, 5, 9, 0])
+    
+    def test_addi(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        addi_1 = ADDI(rd=0, r1=0, imm=0)
+        state = addi_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+    
+    def test_andi(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        andi_1 = ANDI(rd=0, r1=0, imm=0)
+        state = andi_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+    
+    def test_ori(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        ori_1 = ORI(rd=0, r1=0, imm=0)
+        state = ori_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+    
+    def test_xori(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        xori_1 = XORI(rd=0, r1=0, imm=0)
+        state = xori_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+
+    def test_slli(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        slli_1 = SLLI(rd=0, r1=0, imm=0)
+        state = slli_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+
+    def test_srli(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        srli_1 = SRLI(rd=0, r1=0, imm=0)
+        state = srli_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+
+    def test_srai(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        srai_1 = SRAI(rd=0, r1=0, imm=0)
+        state = srai_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+
+    def test_slti(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        slti_1 = SLTI(rd=0, r1=0, imm=0)
+        state = slti_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
+
+    def test_sltiu(self):
+        state = ArchitecturalState(register_file=RegisterFile(registers=[0, 5, 9, 0]))
+
+        sltiu_1 = SLTIU(rd=0, r1=0, imm=0)
+        state = sltiu_1.behavior(state)
+        self.assertEqual(state.register_file.registers, [0, 5, 9, 0])
 
 
 class TestParser(unittest.TestCase):
