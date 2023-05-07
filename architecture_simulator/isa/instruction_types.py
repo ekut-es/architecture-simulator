acb480c66c47be59f3e2c17fe73dff1fb17be416
Yamin Maazou <yamin.maazou@student.uni-tuebingen.de>
2023-05-08 00:24:28 +0200

    fix

--- architecture_simulator/isa/instruction_types.py
+++ architecture_simulator/isa/instruction_types.py
@@ -28,11 +28,6 @@ class ITypeInstruction(Instruction):
             rs1 (int): source register 1
             imm (int): offset to be further progressed by the instruction
         """
-        if imm < -2048 or 2047 < imm:
-            raise ValueError(
-                "I-Type Instruction immediate values have to be in range(-2048, 2048). Given immediate was "
-                + str(imm)
-            )
         super().__init__(**args)
         self.rd = rd
         self.rs1 = rs1
