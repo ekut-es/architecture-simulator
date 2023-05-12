03ad409f31af1fa45f925f5eb039529ba2c816bc
Martin Röhm <martin.roehm@student.uni-tuebingen.de>
2023-05-12 17:23:42 +0000

    moved fibonacci_recursive to tests

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -4,7 +4,6 @@ from architecture_simulator.uarch.architectural_state import RegisterFile
 from architecture_simulator.uarch.architectural_state import Memory
 from architecture_simulator.uarch.architectural_state import ArchitecturalState
 from architecture_simulator.simulation.simulation import Simulation
-from architecture_simulator.examples.examples import fibonacci_recursive
 
 
 class TestSimulation(unittest.TestCase):
@@ -24,15 +23,3 @@ class TestSimulation(unittest.TestCase):
         self.assertEqual(simulation.state.register_file.registers[0], 4)
         # simulation.step_simulation()
         # self.assertEqual(simulation.state.register_file.registers[0], 2)
-
-    def test_fibonacci_recursive(self):
-        self.assertEqual(int(fibonacci_recursive(-5)), 0)
-        self.assertEqual(int(fibonacci_recursive(0)), 0)
-        self.assertEqual(int(fibonacci_recursive(1)), 1)
-        self.assertEqual(int(fibonacci_recursive(2)), 1)
-        self.assertEqual(int(fibonacci_recursive(3)), 2)
-        self.assertEqual(int(fibonacci_recursive(4)), 3)
-        self.assertEqual(int(fibonacci_recursive(5)), 5)
-        self.assertEqual(int(fibonacci_recursive(6)), 8)
-        self.assertEqual(int(fibonacci_recursive(7)), 13)
-        self.assertEqual(int(fibonacci_recursive(20)), 6765)
