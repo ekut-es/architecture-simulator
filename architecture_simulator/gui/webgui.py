cd7f4592171e2d1757f9880cfd19132993df8fd2
Michael Pasch <michael.pasch@student.uni-tuebingen.de>
2023-05-15 00:56:55 +0200

    started working on webgui interface with python

--- architecture_simulator/gui/webgui.py
+++ architecture_simulator/gui/webgui.py
@@ -1,10 +1,11 @@
 import archsim_js
 
-from architecture_simulator.uarch.architectural_state import RegisterFile
+from architecture_simulator.uarch.architectural_state import RegisterFile, Memory
 from architecture_simulator.isa.instruction_types import Instruction
 from architecture_simulator.isa.rv32i_instructions import ADD
 from architecture_simulator.uarch.architectural_state import ArchitecturalState
 from architecture_simulator.simulation.simulation import Simulation
+import fixedint
 
 simulation = None
 
@@ -12,7 +13,21 @@ simulation = None
 def sim_init():
     global simulation
     simulation = Simulation(
-        state=ArchitecturalState(register_file=RegisterFile(registers=[0, 2, 0, 0])),
+        state=ArchitecturalState(
+            register_file=RegisterFile(registers=[0, 2, 0, 0, 4]),
+            memory=Memory(
+                memory_file=dict(
+                    [
+                        (0, fixedint.MutableUInt8(1)),
+                        (1, fixedint.MutableUInt8(2)),
+                        (2, fixedint.MutableUInt8(3)),
+                        (3, fixedint.MutableUInt8(-1)),
+                        (pow(2, 32) - 1, fixedint.MutableUInt8(4)),
+                        (2047, fixedint.MutableUInt8(5)),
+                    ]
+                )
+            ),
+        ),
         instructions={},
     )
 
@@ -20,6 +35,9 @@ def sim_init():
         archsim_js.append_register(
             reg_i, simulation.state.register_file.registers[reg_i]
         )
+
+    for mem_i in simulation.state.memory.memory_file.keys():
+        archsim_js.append_memory(mem_i, int(simulation.state.memory.memory_file[mem_i]))
 
     return simulation
 
@@ -32,4 +50,6 @@ def exec_instr(instr: str):
     simulation.step_simulation()
     for reg_i, reg_val in enumerate(simulation.state.register_file.registers):
         archsim_js.update_register(reg_i, reg_val)
+    for address, address_val in enumerate(simulation.state.memory.memory_file):
+        archsim_js.update_memory(address, address_val)
     return simulation
