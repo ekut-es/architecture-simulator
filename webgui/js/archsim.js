a4590a8caafddaf81826fe4a17dd1a40417c8a4e
Alexander Schoger <alexander.schoger@student.uni-tuebingen.de>
2023-06-12 21:30:01 +0200

    fixed some minor bugs

--- webgui/js/archsim.js
+++ webgui/js/archsim.js
@@ -3,6 +3,7 @@ const registers = document.getElementById("gui_registers_table_id");
 const memory = document.getElementById("gui_memory_table_id");
 const instructions = document.getElementById("gui_cmd_table_body_id");
 const input = document.getElementById("input");
+const loading_screen = document.getElementById("loading_screen");
 
 function addToOutput(s) {
     output.value += ">>>" + input.value + "\n" + s + "\n";
@@ -92,6 +93,7 @@ output.value = "Output \n\nInitializing... ";
 input.value = "add x1, x2, x3"
 // init Pyodide
 async function main() {
+    loading_screen.showModal()
     let pyodide = await loadPyodide();
     await pyodide.loadPackage("micropip");
     const micropip = pyodide.pyimport("micropip");
@@ -102,15 +104,15 @@ from architecture_simulator.gui.webgui import *
 sim_init()
     `);
     output.value += "Ready!\n";
+    loading_screen.close();
     return pyodide;
 }
 let pyodideReadyPromise = main();
 
 async function evaluatePython_step_sim() {
+    loading_screen.showModal()
     let pyodide = await pyodideReadyPromise;
-    //alert("step")
-    //alert(input.value.split("\n"))
-    //alert(simulation_json)
+    loading_screen.close();
     input_str = input.value
     try {
         step_sim = pyodide.globals.get("step_sim");
@@ -123,9 +125,9 @@ async function evaluatePython_step_sim() {
 }
 
 async function evaluatePython_run_sim() {
+    loading_screen.showModal()
     let pyodide = await pyodideReadyPromise;
-    //alert("run")
-    //alert(input.value.split("\n"))
+    loading_screen.close();
     input_str = input.value
     try {
         run_sim = pyodide.globals.get("run_sim");
@@ -137,7 +139,9 @@ async function evaluatePython_run_sim() {
 }
 
 async function evaluatePython_reset_sim() {
+    loading_screen.showModal()
     let pyodide = await pyodideReadyPromise;
+    loading_screen.close();
     registers.innerHTML = ""
     memory.innerHTML = ""
     try {
