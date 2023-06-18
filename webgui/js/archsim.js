cb57ba86b396f03d401d2f2f783499a3a3aed873
Michael Pasch <michael.pasch@student.uni-tuebingen.de>
2023-06-19 00:54:55 +0200

    added some console.logs for testing purposes

--- webgui/js/archsim.js
+++ webgui/js/archsim.js
@@ -101,6 +101,9 @@ async function main() {
     console.log(window.location.protocol)
     console.log(window.location.href)
     console.log(window.location.origin)
+    console.log(window.location.hash)
+    console.log(window.location.host)
+    console.log(window.location.hostname)
     if(window.location.protocol == 'https:')
     {
         console.log("if_https")
