048ed5351cf07a0f4a16083edea65faa330f009b
Michael Kuhn <michael.kuhn@uni-tuebingen.de>
2023-10-19 16:59:36 +0200

    download text in editor on ctrl+s

--- webgui/js/editor.js
+++ webgui/js/editor.js
@@ -22,3 +22,34 @@ const editor_vis = CodeMirror.fromTextArea(
     }
 );
 editor_vis.setSize(null, "57vh");
+
+function saveTextAsFile() {
+    // thanks to https://stackoverflow.com/questions/51315044/how-do-i-save-the-content-of-the-editor-not-the-whole-html-page
+    var textToWrite = editor.getValue();
+    var textFileAsBlob = new Blob([textToWrite], {
+        type: "text/plain;charset=utf-8",
+    });
+    var fileNameToSaveAs = "my_program.asm";
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
+CodeMirror.commands.save = function () {
+    saveTextAsFile();
+};
