1fef7f0c5ba0602f4e19abee692f42cda691883b
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2023-11-29 16:01:03 +0100

    The TOY PC is now actually 12 bit wide

--- architecture_simulator/util/fixedint_12.py
+++ architecture_simulator/util/fixedint_12.py
@@ -1,7 +1,3 @@
 import fixedint
 
-__all__ = []
-cls = fixedint.FixedInt(12, signed=False, mutable=True)
-cls.__module__ = __name__
-__all__ += [cls.__name__]
-globals()[cls.__name__] = cls
+MutableUInt12 = fixedint.FixedInt(12, signed=False, mutable=True)
