a0fd995b78cd3e0d662e96326af5d3e9424dff13
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-01-21 09:04:51 +0000

    Added a svg build script for custom markers

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ python -m build
 # ensure public folder is present and copy python package
 mkdir -p webgui/public/
 cp dist/*.whl webgui/public/
+# ensure custom markers are present in the svg
+python build_svg.py
 # ensure js packages are available and run npm scripts
 npm install
 npm run $1
