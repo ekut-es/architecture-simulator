cb04c605e9d8754cf800dfdf28c16dc5b8849e70
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-01-23 10:29:52 +0100

    Centered the toggle buttons

--- webgui/src/components/riscv/RiscvElementToggle.vue
+++ webgui/src/components/riscv/RiscvElementToggle.vue
@@ -5,11 +5,15 @@ import { riscvSettings } from '@/js/riscv_settings';
 </script>
 
 <template>
-    <div class="btn-group" role="group" aria-label="Toggle visibility of display elements">
-        <ToggleButton v-model="riscvSettings.showInput.value" base-id="riscv-toggle-input">Input</ToggleButton>
-        <ToggleButton v-model="riscvSettings.showMemory.value" base-id="riscv-toggle-memory">Memory</ToggleButton>
-        <ToggleButton v-model="riscvSettings.showRegisters.value" base-id="riscv-toggle-registers">Registers</ToggleButton>
-        <ToggleButton v-model="riscvSettings.showOutput.value" base-id="riscv-toggle-output">Output</ToggleButton>
-        <ToggleButton v-model="riscvSettings.showVisualization.value" base-id="riscv-toggle-visualization">Visualization</ToggleButton>
+    <div class="d-flex justify-content-center">
+        <div class="btn-group" role="group" aria-label="Toggle visibility of display elements">
+            <ToggleButton v-model="riscvSettings.showInput.value" base-id="riscv-toggle-input">Input</ToggleButton>
+            <ToggleButton v-model="riscvSettings.showMemory.value" base-id="riscv-toggle-memory">Memory</ToggleButton>
+            <ToggleButton v-model="riscvSettings.showRegisters.value" base-id="riscv-toggle-registers">Registers
+            </ToggleButton>
+            <ToggleButton v-model="riscvSettings.showOutput.value" base-id="riscv-toggle-output">Output</ToggleButton>
+            <ToggleButton v-model="riscvSettings.showVisualization.value" base-id="riscv-toggle-visualization">Visualization
+            </ToggleButton>
+        </div>
     </div>
 </template>
--- webgui/src/components/toy/ToyElementToggle.vue
+++ webgui/src/components/toy/ToyElementToggle.vue
@@ -5,9 +5,13 @@ import { toySettings } from '@/js/toy_settings';
 </script>
 
 <template>
-    <div class="btn-group" role="group" aria-label="Toggle visibility of display elements">
-        <ToggleButton v-model="toySettings.showInput.value" base-id="toy-toggle-input">Input</ToggleButton>
-        <ToggleButton v-model="toySettings.showMainColumn.value" base-id="toy-toggle-main-column">Registers/Memory/Output</ToggleButton>
-        <ToggleButton v-model="toySettings.showVisualization.value" base-id="toy-toggle-visualization">Visualization</ToggleButton>
+    <div class="d-flex justify-content-center">
+        <div class="btn-group" role="group" aria-label="Toggle visibility of display elements">
+            <ToggleButton v-model="toySettings.showInput.value" base-id="toy-toggle-input">Input</ToggleButton>
+            <ToggleButton v-model="toySettings.showMainColumn.value" base-id="toy-toggle-main-column">
+                Registers/Memory/Output</ToggleButton>
+            <ToggleButton v-model="toySettings.showVisualization.value" base-id="toy-toggle-visualization">Visualization
+            </ToggleButton>
+        </div>
     </div>
 </template>
