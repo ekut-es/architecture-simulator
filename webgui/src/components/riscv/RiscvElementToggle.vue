c009fcfc961526e1589ee38bec54366f92d010d3
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-03-08 08:26:47 +0100

    Made the toggle buttons prettier

--- webgui/src/components/riscv/RiscvElementToggle.vue
+++ webgui/src/components/riscv/RiscvElementToggle.vue
@@ -6,42 +6,48 @@ import { riscvSettings } from "@/js/riscv_settings";
 </script>
 
 <template>
-    <div class="d-flex justify-content-center">
-        <div
-            class="btn-group"
-            role="group"
-            aria-label="Toggle visibility of display elements"
+    <div
+        class="btn-group"
+        role="group"
+        aria-label="Toggle visibility of display elements on the left side"
+    >
+        <ToggleButton
+            v-model="riscvSettings.showInput.value"
+            base-id="riscv-toggle-input"
+            >Input</ToggleButton
         >
-            <ToggleButton
-                v-model="riscvSettings.showInput.value"
-                base-id="riscv-toggle-input"
-                >Input</ToggleButton
-            >
-            <ToggleButton
-                v-model="riscvSettings.showMemory.value"
-                base-id="riscv-toggle-memory"
-                >Memory</ToggleButton
-            >
-            <ToggleButton
-                v-model="riscvSettings.showRegistersOutput.value"
-                base-id="riscv-toggle-registers-output"
-                >Registers/Output
-            </ToggleButton>
-            <ToggleButton
-                v-model="riscvSettings.showVisualization.value"
-                base-id="riscv-toggle-visualization"
-                >Visualization
-            </ToggleButton>
-            <ToggleButton
-                v-model="riscvSettings.showDataCache.value"
-                base-id="riscv-toggle-data-cache"
-                >Data Cache
-            </ToggleButton>
-            <ToggleButton
-                v-model="riscvSettings.showInstructionCache.value"
-                base-id="riscv-toggle-instruction-cache"
-                >Instruction Cache
-            </ToggleButton>
-        </div>
+        <ToggleButton
+            v-model="riscvSettings.showMemory.value"
+            base-id="riscv-toggle-memory"
+            >Memory</ToggleButton
+        >
+        <ToggleButton
+            v-model="riscvSettings.showRegistersOutput.value"
+            base-id="riscv-toggle-registers-output"
+            >Registers/Output
+        </ToggleButton>
+    </div>
+    <div
+        class="btn-group"
+        role="group"
+        aria-label="Toggle visibility of the display elements on the right side"
+    >
+        <ToggleButton
+            v-model="riscvSettings.showVisualization.value"
+            base-id="riscv-toggle-visualization"
+            >Visualization
+        </ToggleButton>
+        <ToggleButton
+            v-if="riscvSettings.dataCache.value.enable"
+            v-model="riscvSettings.showDataCache.value"
+            base-id="riscv-toggle-data-cache"
+            >Data Cache
+        </ToggleButton>
+        <ToggleButton
+            v-if="riscvSettings.instructionCache.value.enable"
+            v-model="riscvSettings.showInstructionCache.value"
+            base-id="riscv-toggle-instruction-cache"
+            >Instruction Cache
+        </ToggleButton>
     </div>
 </template>
