de7df251f17fcde727cb46a35654ae8f63db8ee3
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-03-20 08:53:35 +0100

    Added a custom error for misaligned cache access Fixes #89

--- architecture_simulator/util/integer_manipulation.py
+++ architecture_simulator/util/integer_manipulation.py
@@ -7,6 +7,15 @@ if TYPE_CHECKING:
     from architecture_simulator.uarch.memory.decoded_address import DecodedAddress
 
 
+class ByteOffsetError(ValueError):
+    def __init__(self, offset, max) -> None:
+        self.offset = offset
+        self.max_offset = max
+
+    def __repr__(self) -> str:
+        return f"The specified offset of {self.offset} byte{'' if self.offset == 1 else 's'} is illegal since the operation would cross a word boundary. The maximum allowed offset is {self.max_offset} bytes."
+
+
 def byte_from_block(decoded_address: DecodedAddress, block: list[UInt32]) -> UInt8:
     return UInt8(
         int(block[decoded_address.block_offset]) >> (decoded_address.byte_offset * 8)
@@ -14,14 +23,16 @@ def byte_from_block(decoded_address: DecodedAddress, block: list[UInt32]) -> UIn
 
 
 def halfword_from_block(decoded_address: DecodedAddress, block: list[UInt32]) -> UInt16:
-    assert decoded_address.byte_offset <= 2  # TODO: Custom Error
+    if decoded_address.byte_offset > 2:
+        raise ByteOffsetError(decoded_address.byte_offset, 2)
     return UInt16(
         int(block[decoded_address.block_offset]) >> (decoded_address.byte_offset * 8)
     )
 
 
 def word_from_block(decoded_address: DecodedAddress, block: list[UInt32]) -> UInt32:
-    assert decoded_address.byte_offset == 0  # TODO: Custom Error
+    if decoded_address.byte_offset != 0:
+        raise ByteOffsetError(decoded_address.byte_offset, 0)
     return block[decoded_address.block_offset]
 
 
@@ -38,7 +49,8 @@ def byte_into_block(
 def halfword_into_block(
     decoded_address: DecodedAddress, block: list[UInt32], halfword: UInt16
 ) -> list[UInt32]:
-    assert decoded_address.byte_offset <= 2  # TODO: Custom Error
+    if decoded_address.byte_offset > 2:
+        raise ByteOffsetError(decoded_address.byte_offset, 2)
     word = int(block[decoded_address.block_offset])
     word = word & (~(0xFFFF << (decoded_address.byte_offset * 8)))
     word = word | (int(halfword) << (decoded_address.byte_offset * 8))
@@ -49,6 +61,7 @@ def halfword_into_block(
 def word_into_block(
     decoded_address: DecodedAddress, block: list[UInt32], word: UInt32
 ) -> list[UInt32]:
-    assert decoded_address.byte_offset == 0  # TODO: Custom Error
+    if decoded_address.byte_offset != 0:
+        raise ByteOffsetError(decoded_address.byte_offset, 0)
     block[decoded_address.block_offset] = word
     return block
