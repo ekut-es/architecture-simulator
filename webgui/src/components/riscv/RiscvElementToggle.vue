9067aef296507e554c440abb6557dc75daf55f71
Richard Müller <ri.mueller@student.uni-tuebingen.de>
2024-03-22 11:09:06 +0100

    Changed the layout inside the control bar

--- webgui/src/components/riscv/RiscvElementToggle.vue
+++ webgui/src/components/riscv/RiscvElementToggle.vue
@@ -1,35 +1,8 @@
 <!-- The toggle button group for toggling the riscv elements. -->
 <script setup>
-import { watch } from "vue";
 import ToggleButton from "../ToggleButton.vue";
 
 import { riscvSettings } from "@/js/riscv_settings";
-
-// Select the processor view if the user disables the cache he is currently viewing.
-watch(
-    () => [
-        riscvSettings.dataCache.value.enable,
-        riscvSettings.instructionCache.value.enable,
-        riscvSettings.dataCacheTooBig.value,
-        riscvSettings.instructionCacheTooBig.value,
-    ],
-    ([
-        enableDataCache,
-        enableInstructionCache,
-        dataCacheTooBig,
-        instructionCacheTooBig,
-    ]) => {
-        const selected = riscvSettings.visContainerSelection.value;
-        if (
-            ((!enableDataCache || dataCacheTooBig) &&
-                selected === "Data Cache") ||
-            ((!enableInstructionCache || instructionCacheTooBig) &&
-                selected === "Instruction Cache")
-        ) {
-            riscvSettings.visContainerSelection.value = "Processor";
-        }
-    }
-);
 </script>
 
 <template>
@@ -54,27 +27,4 @@ watch(
             >Registers/Output
         </ToggleButton>
     </div>
-    <select
-        v-model="riscvSettings.visContainerSelection.value"
-        aria-label="Select the element to be displayed on the right side"
-    >
-        <option>None</option>
-        <option>Processor</option>
-        <option
-            v-if="
-                riscvSettings.dataCache.value.enable &&
-                !riscvSettings.dataCacheTooBig.value
-            "
-        >
-            Data Cache
-        </option>
-        <option
-            v-if="
-                riscvSettings.instructionCache.value.enable &&
-                !riscvSettings.instructionCacheTooBig.value
-            "
-        >
-            Instruction Cache
-        </option>
-    </select>
 </template>
