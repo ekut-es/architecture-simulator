2fbce8074aea456e0bb7fa98383d8dfc2ec229c5
Michael Kuhn <michael.kuhn@uni-tuebingen.de>
2024-04-24 12:29:46 +0200

    update version string

--- webgui/src/main.js
+++ webgui/src/main.js
@@ -1,7 +1,7 @@
 import "./scss/styles.scss";
 import "./css/main.css";
 import "./css/splitjs.css";
-import architectureSimulatorPackageUrl from "../../dist/architecture_simulator-1.3.0-py3-none-any.whl";
+import architectureSimulatorPackageUrl from "../../dist/architecture_simulator-1.3.1.dev0-py3-none-any.whl";
 
 import { createApp } from "vue";
 
